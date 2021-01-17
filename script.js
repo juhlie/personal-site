1362d6a2aadf549e8d6ec6f1f507d23a4a510b19
Julie Lam <43072938+blouhie@users.noreply.github.com>
2021-01-17 16:57:52 -0500

    easter egg hehe

---
+++ script.js
@@ -0,0 +1,20 @@
+const day = new Date().getDay();
+const days = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+console.log(`
+✧･ﾟ: *✧･ﾟ*･ﾟ✧*:･ﾟ✧･ﾟ: *✧･ﾟ･ﾟ✧*:･ﾟ✧✧･ﾟ: *✧･ﾟ･ﾟ✧*:･ﾟ✧
+
+why hello there curious creature ~(=^‥^)ﾉ◎～
+			 
+	how do you do on this fine ${days[day]}? 
+
+nothing to see here	ฅ^•ﻌ•^ฅ 
+
+✧･ﾟ: *✧･ﾟ*･ﾟ✧*:･ﾟ✧･ﾟ: *✧･ﾟ･ﾟ✧*:･ﾟ✧✧･ﾟ: *✧･ﾟ･ﾟ✧*:･ﾟ✧`);
