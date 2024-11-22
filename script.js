9b003e04ad5ff293b7ea14194d7810d7cb680c49
Glitch (julielam) <none>
2024-11-22 06:16:41 +0000

    🐴🤿 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,20 @@
+const day = new Date().getDay();
+const days = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+console.log(`
+✧･ﾟ: *✧･ﾟ*･ﾟ✧*:･ﾟ✧･ﾟ: *✧･ﾟ･ﾟ✧*:･ﾟ✧✧･ﾟ: *✧･ﾟ･ﾟ✧*:･ﾟ✧
+
+why hello there curious creature ʕ ˵• ₒ •˵ ʔ
+			 
+ʕ •ᴥ•ʔゝ how do you do on this fine ${days[day]}? 
+
+nothing to see here	ʕ•ᴥ•ʔﾉ ♡ ┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴
+
+✧･ﾟ: *✧･ﾟ*･ﾟ✧*:･ﾟ✧･ﾟ: *✧･ﾟ･ﾟ✧*:･ﾟ✧✧･ﾟ: *✧･ﾟ･ﾟ✧*:･ﾟ✧`);
