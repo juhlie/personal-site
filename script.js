5aeca46a69c23ee993239ab8d46319ba09eed3a9
Julie Lam <43072938+juhlie@users.noreply.github.com>
2025-06-05 15:47:10 -0400

    make phone div draggable

--- script.js
+++ script.js
@@ -18,3 +18,45 @@ why hello there curious creature ʕ ˵• ₒ •˵ ʔ
 nothing to see here	ʕ•ᴥ•ʔﾉ ♡ ┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴
 
 ✧･ﾟ: *✧･ﾟ*･ﾟ✧*:･ﾟ✧･ﾟ: *✧･ﾟ･ﾟ✧*:･ﾟ✧✧･ﾟ: *✧･ﾟ･ﾟ✧*:･ﾟ✧`);
+
+/**
+ *
+ * phone redirect drag
+ *
+ */
+
+const phone = document.querySelector("#redirect");
+
+// position phone in the center of the screen to start
+phone.style.left = "50%";
+phone.style.top = "50%";
+phone.style.transform = "translate(-50%, -50%)";
+
+phone.addEventListener("mousedown", (event) => grabPhone(event));
+
+function grabPhone(event) {
+  event.preventDefault();
+  phone.style.left = phone.getBoundingClientRect().x + "px";
+  phone.style.top = phone.getBoundingClientRect().y + "px";
+  phone.style.transform = "translate(0px, 0px)";
+  drag(event.clientX, event.clientY);
+}
+
+function drag(startX, startY) {
+  phone.onmousemove = movePhone;
+
+  phone.onmouseup = () => {
+    phone.onmousemove = null;
+  };
+
+  window.onmouseout = () => {
+    phone.onmousemove = null;
+  };
+
+  function movePhone(event) {
+    event.preventDefault();
+    const deltaX = event.clientX - startX;
+    const deltaY = event.clientY - startY;
+    phone.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+  }
+}
