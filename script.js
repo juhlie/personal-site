a89cb3e396ad089e476c99dc46ef3544ed987b5f
Julie Lam <43072938+juhlie@users.noreply.github.com>
2025-06-10 03:08:02 -0400

    make redirect draggable on mobile

--- script.js
+++ script.js
@@ -32,31 +32,39 @@ phone.style.left = "50%";
 phone.style.top = "50%";
 phone.style.transform = "translate(-50%, -50%)";
 
-phone.addEventListener("mousedown", (event) => grabPhone(event));
+phone.addEventListener("pointerdown", grabPhone);
 
 function grabPhone(event) {
   event.preventDefault();
   phone.style.left = phone.getBoundingClientRect().x + "px";
   phone.style.top = phone.getBoundingClientRect().y + "px";
-  phone.style.transform = "translate(0px, 0px)";
+  phone.style.transform = "translate(0px, 0px) scale(1.05)";
   drag(event.clientX, event.clientY);
 }
 
 function drag(startX, startY) {
-  phone.onmousemove = movePhone;
-
-  phone.onmouseup = () => {
-    phone.onmousemove = null;
-  };
-
-  window.onmouseout = () => {
-    phone.onmousemove = null;
-  };
-
   function movePhone(event) {
     event.preventDefault();
     const deltaX = event.clientX - startX;
     const deltaY = event.clientY - startY;
-    phone.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+    phone.style.transform = `translate(${deltaX}px, ${deltaY}px) scale(1.05)`;
   }
+
+  function stopDrag() {
+    phone.style.left = phone.getBoundingClientRect().x + "px";
+    phone.style.top = phone.getBoundingClientRect().y + "px";
+    phone.style.transform = "translate(0px, 0px)";
+    phone.removeEventListener("pointermove", movePhone);
+    phone.removeEventListener("pointerup", stopDrag);
+    document.removeEventListener("pointerup", stopDrag);
+    phone.removeEventListener("pointercancel", stopDrag);
+    document.removeEventListener("pointercancel", stopDrag);
+  }
+
+  phone.addEventListener("pointermove", movePhone);
+  phone.addEventListener("pointerup", stopDrag);
+  document.addEventListener("pointerup", stopDrag);
+  phone.addEventListener("pointercancel", stopDrag);
+  document.addEventListener("pointercancel", stopDrag);
+  phone.addEventListener("contextmenu", (e) => e.preventDefault());
 }
